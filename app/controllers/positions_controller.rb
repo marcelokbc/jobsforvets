b06d2db619960fc6c47acff2dd055aa8bb203f41
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-01-24 22:12:44 -0300

    I've added forms and their actions to positions, fixing the bug when creating a position

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,13 +1,66 @@
 class PositionsController < ApplicationController
+  before_action :set_company, :set_I18n_careers, :set_I18n_contracts
+  before_action :set_position, only: [:edit, :show, :update]
+
   def index
-  end
-
-  def new
-  end
-
-  def edit
+    @positions = @company.positions
+    # @pagy, @positions = pagy(@positions)
   end
 
   def show
+    render :form
   end
+
+  def new
+    @position = @company.positions.new
+    render :form
+  end
+
+  def create
+    @position = @company.positions.new(params_position)
+    if @position.save
+      flash[:notice] = "Vaga cadastrada com sucesso!"
+      redirect_to positions_path
+    else
+      render :form, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    render :form
+  end
+
+  def update
+    if @position.update(params_position)
+      flash[:notice] = "Vaga atualizada com sucesso!"
+      redirect_to positions_path
+    else
+      render :form, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+    def set_company
+      redirect_to new_company_path if current_user.company.nil?
+      @company = current_user.company
+    end
+
+    def set_position
+      @position = @company.positions.find(params[:id])
+    end
+
+    def set_I18n_careers
+      @careers = I18n.t('activerecord.attributes.position.careers')
+    end
+
+    def set_I18n_contracts
+      @contracts = I18n.t('activerecord.attributes.position.contracts')
+    end
+
+    def params_position
+      params.require(:position).permit(:name, :career, :contract, :remote, :city, :state, :summary, :description, :publish)
+    end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,9 @@
 class Position < ApplicationRecord
   belongs_to :company
+
+  enum career: [:general_practitioners, :surgery, :ortophdics, :dermatology, :cardiology, 
+                :ophthalmology, :nutriton, :other]
+  enum contract: [:clt, :pj, :match]
+
+  validates :name, :career, :contract, :city, :state, :summary, :description, presence: true
 end
