3c6d2ccd7802f1f9f0568fdf76dd077a6e279ae7
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-01-30 16:34:06 -0300

    Adding pagy, partial position and adjustment view home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @positions = Position.all
+    @pagy, @positions = pagy(@positions)
     if user_signed_in?
       render 'index_logged_in'
     else
