41c32c62f472e02f70611bacb15d0a476ae7ede8
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-02-05 18:18:07 -0300

    Adding view public position and show position, I removed the logic of the user not logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,10 @@ class HomeController < ApplicationController
     @contracts = [["CLT", 0 ], ["PJ", 1], ["A combinar", 2]]
     @careers = [["Clínico Geral", 0], ["Cirurgia", 1], ["Ortopedia", 2], ["Dermatologia", 3], ["Cardiologia", 4], ["Oftalmologia", 5], ["Nutrição", 6], ["Outro", 7]]
     @pagy, @positions = pagy(@positions)
-    if user_signed_in?
-      render 'index_logged_in'
-    else
-      render 'index_not_logged_in'
-    end
+    # if user_signed_in?
+    #   render 'index_logged_in'
+    # else
+    #   render 'index_not_logged_in'
+    # end
   end
 end
