196ef87c4cfd3e55e2b3e4204f2a6941f9b24886
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-02-06 10:56:36 -0300

    Adding form applicant inside public position

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -4,4 +4,20 @@ class ApplicantsController < ApplicationController
 
   def new
   end
+
+  def create
+    @applicant = current_user.applicants.new(applicant_params)
+    if @applicant.save
+      flash[:notice] = "Você se candidatou a vaga com sucesso!"
+    else
+      flash[:alert] = "Houve um erro ao se candidatar a vaga! Tente novamente."
+    end
+    redirect_to public_position_path(@applicant.position.slug)
+  end
+  
+  private 
+
+  def applicant_params
+    params.require(:applicant).permit(:name, :email, :phone, :position_id)
+  end
 end
