93f58ead86189c28ccb233d3d8e7ecbad1ca4bb6
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-02-08 15:55:52 -0300

    Adding seed and adjusments pages

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,21 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    document.addEventListener('turbo:load', () => {
-      document.querySelectorAll('.copy-url').forEach((element) => {
-        element.addEventListener('click', (e) => {
-          e.preventDefault();
-          var url = element.getAttribute('data-clipboard-text');
-          navigator.clipboard.writeText(url)
-            .then(() => {
-              console.log('URL copiado com sucesso');
-            })
-            .catch((error) => {
-              console.error('Erro ao copiar URL', error);
-              console.log('URL copiado com sucesso');
-            });
+    this.element.addEventListener('click', (e) => {
+      e.preventDefault();
+      var url = this.element.getAttribute('data-clipboard-text');
+      navigator.clipboard.writeText(url)
+        .then(() => {
+          alert('URL copiado com sucesso!');
+        })
+        .catch((error) => {
+          alert('Erro ao copiar URL!');
         });
-      });
     });
   }
 }
