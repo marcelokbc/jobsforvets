87d983652b1845233c1ce79665ddb1005ad2618b
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-02-22 16:13:23 -0300

    Adding resume to applicants with exported csv

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,6 +1,31 @@
+require 'csv'
+
 class Applicant < ApplicationRecord
   belongs_to :user
   belongs_to :position
+  has_one_attached :resume
 
-  validates :name, :email, :phone, :position_id, presence: true
+  validates :name, :email, :phone, :position_id, :resume, presence: true
+
+  validate :correct_resume_mime_type
+
+  def self.as_csv
+    attributes = %w{id name email phone}
+
+    CSV.generate(headers: true) do |csv|
+      csv.add_row attributes
+
+      all.each do |applicant|
+        csv.add_row attributes.map { |attr| applicant.send(attr) }
+      end
+    end
+  end
+
+  private
+
+  def correct_resume_mime_type
+    if resume.attached? && !resume.content_type.in?(%w(application/pdf))
+      errors.add(:resume, 'Foi enviado um arquivo com formato inválido. Por favor, envie um arquivo PDF.')
+    end
+  end
 end
