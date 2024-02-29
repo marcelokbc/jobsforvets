4f8651c03b33fb692fd5ea1a296fdb4e68fd4f4e
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-02-29 10:34:17 -0300

    Adding mask field phone form applicant

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,8 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@3.0.4/dist/tailwindcss-stimulus-components.modern.js"
+pin "imask", to: "https://ga.jspm.io/npm:imask@6.4.2/esm/index.js"
+pin "alpinejs", to: "https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
