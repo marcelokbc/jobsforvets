483959e4829df0545f98da483583d9a14b8a7a48
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-03-21 11:22:51 -0300

    Adjustment pundit to company and admin

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @company = current_user.build_company
   end
@@ -30,15 +32,21 @@ class CompaniesController < ApplicationController
   end
 
   def dashboard
+    authorize :company, :dashboard?
     @company = current_user.company
     @positions = @company.positions
     @positions_without_applicants = @positions.select { |position| position.applicants.empty? }
     @positions_with_applicants = @positions.select { |position| position.applicants.any? }
-    @applicants = @positions_with_applicants.map { |position| position.applicants }.flatten
+    @applicants = @positions_with_applicants.map { |position| position.applicants }.flatten 
   end
 
   private
   def company_params
     params.require(:company).permit(:name, :url, :logo)
+  end
+
+  def user_not_authorized
+    flash[:alert] = "Você não está autorizado a performar essa ação."
+    redirect_back(fallback_location: root_path)
   end
 end
